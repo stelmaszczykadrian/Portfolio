3ce0fcb3de4b1451297533d02446cd6365ace643
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-10-04 21:00:45 +0200

    Feature: Create Data component for Homepage

---
+++ portfolio/src/components/home/Data.jsx
@@ -0,0 +1,17 @@
+export default function Data() {
+
+    return (
+        <div className="home__data">
+            <h1 className="home__title">
+                Adrian Stelmaszczyk
+            </h1>
+            <h3 className="home__subtitle">Junior Full Stack Developer</h3>
+            <p className="home__description">
+                I am a highly motivated Full Stack Developer based in Warsaw, who is passionate and committed to my projects.
+            </p>
+           <a href="#contact" className="button button--flex">
+               Say Hello!
+           </a>
+        </div>
+    );
+}
