063da7966d088c64fa8d8e710ec4611bd90efd7d
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-10-06 18:30:10 +0200

    Feature: Add Data component with projects data

---
+++ portfolio/src/components/Work/Data.jsx
@@ -0,0 +1,30 @@
+import Gemora from '../../assets/workimages/Gemora.jpg';
+import JobFever from '../../assets/workimages/JobFever.jpg';
+import DungeonAdventures from '../../assets/workimages/DungeonAdventures.jpg';
+import KampShop from '../../assets/workimages/KampShop.jpg';
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Gemora,
+        title: 'Gemora',
+
+
+    },
+    {
+        id: 2,
+        image: JobFever,
+        title: 'Job Fever',
+    },
+    {
+        id: 3,
+        image: DungeonAdventures,
+        title: 'Dungeon Adventures',
+    },
+    {
+        id: 4,
+        image: KampShop,
+        title: 'Kamp Shop',
+    },
+
+]
