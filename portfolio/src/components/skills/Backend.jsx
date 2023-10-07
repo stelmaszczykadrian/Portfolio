52798c974f3a4d5147e324bbb6fcb795e8deea25
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-10-07 09:44:04 +0200

    Refactor: Refactor Backend component to use grouped skills data

--- portfolio/src/components/skills/Backend.jsx
+++ portfolio/src/components/skills/Backend.jsx
@@ -1,67 +1,21 @@
+import Skill from "./Skill";
+import {backendSkills} from "./Data";
+import {groupSize, splitSkillsIntoGroups} from "../../util/util";
 
 export default function Backend() {
+    const groupedSkills = splitSkillsIntoGroups(backendSkills, groupSize);
     return (
         <div className="skills__content">
             <h3 className="skills__title">Backend Developer</h3>
             <div className="skills__box">
-                <div className="skills__group">
-                    <div className="skills__data">
-                        <i className="bx bx-badge-check"></i>
-
-                        <div>
-                            <h3 className="skills__name">HTML</h3>
-                            <span className="skills__level">Basic</span>
-                        </div>
+                {groupedSkills.map((group, groupIndex) => (
+                    <div key={groupIndex} className="skills__group">
+                        {group.map((skill, index) => (
+                            <Skill key={index} name={skill.name} />
+                        ))}
                     </div>
-                    <div className="skills__data">
-                        <i className="bx bx-badge-check"></i>
-
-                        <div>
-                            <h3 className="skills__name">HTML</h3>
-                            <span className="skills__level">Basic</span>
-                        </div>
-                    </div>
-                    <div className="skills__data">
-                        <i className="bx bx-badge-check"></i>
-
-                        <div>
-                            <h3 className="skills__name">HTML</h3>
-                            <span className="skills__level">Basic</span>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="skills__group">
-                    <div className="skills__data">
-                        <i className="bx bx-badge-check"></i>
-
-                        <div>
-                            <h3 className="skills__name">HTML</h3>
-                            <span className="skills__level">Basic</span>
-                        </div>
-                    </div>
-                    <div className="skills__data">
-                        <i className="bx bx-badge-check"></i>
-
-                        <div>
-                            <h3 className="skills__name">HTML</h3>
-                            <span className="skills__level">Basic</span>
-                        </div>
-                    </div>
-                    <div className="skills__data">
-                        <i className="bx bx-badge-check"></i>
-
-                        <div>
-                            <h3 className="skills__name">HTML</h3>
-                            <span className="skills__level">Basic</span>
-                        </div>
-                    </div>
-                </div>
-
-
-
+                ))}
             </div>
-
         </div>
     );
 }
