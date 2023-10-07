10d4b8fae685fdb56ae020f1ad6fe1756c73d455
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-10-07 09:38:55 +0200

    Feature: Add backendSkills and frontendSkills variable

---
+++ portfolio/src/components/skills/Data.jsx
@@ -0,0 +1,21 @@
+export const backendSkills = [
+    { name: "Java" },
+    { name: "Spring Boot" },
+    { name: "Maven" },
+    { name: "REST" },
+    { name: "Python" },
+    { name: "Spring Framework" },
+    { name: "Hibernate, JDBC" },
+    { name: "SQL" },
+];
+
+export const frontendSkills = [
+    { name: "Typescript" },
+    { name: "React" },
+    { name: "Bootstrap" },
+    { name: "Styled Components" },
+    { name: "JavaScript" },
+    { name: "HTML" },
+    { name: "CSS" },
+    { name: "Mui, Chakra" },
+];
