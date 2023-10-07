730678d8140af025568690dc1cd997a05d7dbc06
stelmaszczykadrian <stelmaszczykadrian@gmail.com>
2023-10-07 16:35:17 +0200

    Feature: Add feature to open GitHub repository on image click

--- portfolio/src/components/Work/Data.jsx
+++ portfolio/src/components/Work/Data.jsx
@@ -8,23 +8,25 @@ export const projectsData = [
         id: 1,
         image: Gemora,
         title: 'Gemora',
-
-
+        repository: 'https://github.com/stelmaszczykadrian/Gemora-frontend',
     },
     {
         id: 2,
         image: JobFever,
         title: 'Job Fever',
+        repository: 'https://github.com/stelmaszczykadrian/jobFever-frontend',
     },
     {
         id: 3,
         image: DungeonAdventures,
         title: 'Dungeon Adventures',
+        repository: 'https://github.com/stelmaszczykadrian/Dungeon-Adventures',
     },
     {
         id: 4,
         image: KampShop,
         title: 'Kamp Shop',
+        repository: 'https://github.com/stelmaszczykadrian/KAMP-shop-2.0',
     },
 
 ]
--- portfolio/src/components/Work/WorksItem.jsx
+++ portfolio/src/components/Work/WorksItem.jsx
@@ -1,10 +1,11 @@
 export default function WorksItem({item}) {
-
     return (
         <div className="work__card" key={item.id}>
-            <img src={item.image} alt="" className="work__img"/>
+            <a href={item.repository}>
+                <img src={item.image} alt="" className="work__img"/>
+            </a>
             <h3 className="work__title">{item.title}</h3>
-            <a href="" className="work__button">
+            <a href={item.repository} className="work__button">
                 Demo
                 <i className="bx bx-right-arrow-alt work__button-icon"></i>
             </a>
